d832d983b8878da5c56c7039976f97dd0ba23ca1
Matias Kinnunen <github@mtsknn.fi>
2021-12-01 10:40:49 +0200

    Initialize Leiningen project in Cursive IDE

---
+++ README.md
@@ -0,0 +1,8 @@
+# advent-of-code.clj
+
+[Advent of Code 2021](https://adventofcode.com/2021)
+solutions in Clojure.
+
+## License
+
+MIT &copy; Matias Kinnunen
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject advent-of-code "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2021 solutions in Clojure."
+  :url "https://github.com/mtsknn/advent-of-code.clj"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns advent-of-code.core})
