b884ee7d99022adda6ef5fa4ffdc470c547ea43d
Matias Kinnunen <github@mtsknn.fi>
2021-12-01 21:53:41 +0200

    Add 2021/01

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # advent-of-code.clj
 
-[Advent of Code 2021](https://adventofcode.com/2021)
-solutions in Clojure.
+**[Advent of Code 2021](https://adventofcode.com/2021) solutions in Clojure.**
+
+I don't really know what I'm doing
+(I'm not proficient in Clojure),
+so don't try to learn from my solutions. 😃
+
+## Solutions
+
+The files are meant to be evaluated in a REPL a small piece at a time.
+Running a full file from the CLI probably won't work.
+
+- [2021/01.clj](./src/advent_of_code/2021/01.clj)
 
 ## License
 
---
+++ src/advent_of_code/2021/01.clj
@@ -0,0 +1,45 @@
+(ns advent-of-code.2021.01
+  (:require [clojure.string :as str]))
+
+(def test-data
+  [199 200 208 210 200 207 240 269 260 263])
+
+(def data
+  (->> "src/advent_of_code/2021/01-input.txt"
+       slurp
+       str/split-lines
+       (map #(Integer/parseInt %))))
+
+;; The `:input` and `:output` show example input and output.
+;; The values are evaluated but discarded,
+;; so they don't affect the Clojure program in any way.
+;; I learned the trick from Fred Overflow:
+;; https://youtu.be/orFYFwiG1dM
+:input [1 2 3 4]
+:output '((1 2) (2 3) (3 4))
+(defn create-pairs [numbers]
+  (partition 2 1 numbers))
+
+:input [1 2]
+:output true
+(defn increasing? [[a b]]
+  (> b a))
+
+(def part-1-result
+  (->> data
+       create-pairs
+       (filter increasing?)
+       count))
+
+:input [1 2 3 4 5]
+:output '((1 2 3) (2 3 4) (3 4 5))
+(defn create-groups-of-3 [numbers]
+  (partition 3 1 numbers))
+
+(def part-2-result
+  (->> data
+       create-groups-of-3
+       (map #(apply + %))
+       create-pairs
+       (filter increasing?)
+       count))
