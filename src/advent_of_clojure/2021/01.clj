8b1ee75ea8aecbb6c4f1a2550b535c65815a7850
Matias Kinnunen <github@mtsknn.fi>
2021-12-01 22:25:19 +0200

    Remove puzzle input from repo

--- src/advent_of_clojure/2021/01.clj
+++ src/advent_of_clojure/2021/01.clj
@@ -6,7 +6,7 @@
   [199 200 208 210 200 207 240 269 260 263])
 
 (def data
-  (->> "src/advent_of_clojure/2021/01-input.txt"
+  (->> "resources/inputs/2021/01.txt"
        slurp
        str/split-lines
        (map #(Integer/parseInt %))))
